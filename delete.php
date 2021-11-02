79958e911a9c19fe8e6a70dfbffaf3824ea8bfc0
MI2B-RYAN <ryanz.c69@gmail.com>
2021-11-02 15:50:11 +0700

    master

---
+++ delete.php
@@ -0,0 +1,18 @@
+<!--
+Author : Aguzrybudy
+Created : Selasa, 08-Novermber-2016
+Title : Crud Php Mysqli Dilengkapi dengan upload gambar dan ckeditor
+-->
+<?php 
+   $hapus=$mysqli->query("select*from mahasiswa where id='$_GET[id]'");
+    // memilih gambar untuk dihapus
+    $nama_gambar=mysqli_fetch_array($hapus);
+    // nama field gambar
+    $lokasi=$nama_gambar['gambar'];
+    // alamat tempat gambar
+    $hapus_gambar="images/$lokasi";
+    // script delete gambar dari folder
+    unlink($hapus_gambar);
+    $mysqli->query("DELETE FROM mahasiswa WHERE id='$_GET[id]'");
+    header('location:index.php');
+?>
